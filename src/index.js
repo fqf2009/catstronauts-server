b52d19dc36b53ff7a9741dfc4f8c9da6dc7b6429
Frank Feng <fqf2009@gmail.com>
2023-03-19 12:06:05 -0700

    listen to port denoted by PORT env variable

--- src/index.js
+++ src/index.js
@@ -1,29 +1,32 @@
-const { ApolloServer } = require("@apollo/server");
-const { startStandaloneServer } = require("@apollo/server/standalone");
-const typeDefs = require("./schema");
-const resolvers = require("./resolvers");
-const TrackAPI = require("./datasources/track-api");
+const { ApolloServer } = require("@apollo/server")
+const { startStandaloneServer } = require("@apollo/server/standalone")
+const typeDefs = require("./schema")
+const resolvers = require("./resolvers")
+const TrackAPI = require("./datasources/track-api")
 
 async function startApolloServer() {
   const server = new ApolloServer({
     typeDefs,
-    resolvers,
-  });
+    resolvers
+  })
 
   const { url } = await startStandaloneServer(server, {
     context: async () => {
       return {
         dataSources: {
-          trackAPI: new TrackAPI(),
-        },
-      };
+          trackAPI: new TrackAPI()
+        }
+      }
     },
-  });
+    listen: {
+      port: process.env.PORT || 4000
+    }
+  })
 
   console.log(`
       🚀  Server is running
       📭  Query at ${url}
-    `);
+    `)
 }
 
-startApolloServer();
+startApolloServer()
